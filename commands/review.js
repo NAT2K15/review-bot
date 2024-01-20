671bccc56b2f588aeaaffa6278e68a50f63af4a7
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2024-01-19 22:29:23 -0500

    Changed discord.js v12 to v13 :+1:

--- commands/review.js
+++ commands/review.js
@@ -1,72 +1,38 @@
-const config = require('../config.json');
-const chalk = require('chalk');
 const { MessageEmbed } = require('discord.js');
-module.exports.run = async(nat2k15, message, args) => {
-    let firstchannel = message.guild.channels.cache.get(config.review.allowed_channels);
-    let lastchannel = message.guild.channels.cache.get(config.review.review_sent);
-    let clientrole = message.guild.roles.cache.get(config.review.client_role);
-    if (!firstchannel) {
-        message.channel.send(`Contact the bot owner and tell him to check console`)
-        console.log(chalk.red `[ERROR] ` + chalk.white `You are missing a channel ID in the config or it's invaild`)
-    } else {
-        if (!lastchannel) {
-            message.channel.send(`Contact the bot owner and tell him to check console`)
-            console.log(chalk.red `[ERROR] ` + chalk.white `You are missing a channel ID in the config or it's invaild`)
-        } else {
-            if (!clientrole) {
-                message.channel.send(`Contact the bot owner and tell him to check console`)
-                console.log(chalk.red `[ERROR] ` + chalk.white `You are missing a role ID in the config or it's invaild`)
-            } else {
-                if (message.channel.id !== firstchannel.id) {
-                    let e1 = new MessageEmbed()
-                        .setDescription(`You cannot use this command in this channel! it can only be used in ${firstchannel}`)
-                        .setColor(config.embed.color)
-                        .setFooter(`Made by NAT2K15`)
-                    message.channel.send(e1).then(msg => msg.delete({ timeout: 25000 }))
-                } else {
-                    if (!message.member.roles.cache.has(clientrole.id)) {
-                        let e2 = new MessageEmbed()
-                            .setDescription(`You cannot use this command!. You are not a client`)
-                            .setColor(config.embed.color)
-                        message.channel.send(e2).then(msg => msg.delete({ timeout: 25000 }))
-                    } else {
-                        message.channel.send(`What was the product you purchased`)
-                        message.channel.awaitMessages(m => m.author.id == message.author.id, { max: 1 }).then(firstmsg => {
-                            if (firstmsg.first().content.toLowerCase() !== undefined)
-                                var product = firstmsg.first().content;
-                            message.channel.send(`Please input your review`)
-                            message.channel.awaitMessages(m => m.author.id == message.author.id, { max: 1 }).then(secondmsg => {
-                                if (secondmsg.first().content.toLowerCase() !== undefined)
-                                    var review = secondmsg.first().content;
-
-                                message.channel.send(`What would you rate the product out of ten?`)
-                                message.channel.awaitMessages(m => m.author.id == message.author.id, { max: 1 }).then(foruthgmsg => {
-                                    if (foruthgmsg.first().content.toLowerCase() !== undefined)
-                                        var rate = foruthgmsg.first().content;
-
-                                    let e1 = new MessageEmbed()
-                                        .setTitle(`A review has been made by ${message.author.tag}`)
-                                        .addField(`The Product`, product)
-                                        .addField(`Review`, review)
-                                        .addField(`The product it self out of ten`, rate)
-                                        .setThumbnail(message.author.displayAvatarURL({ format: `png`, dynamic: true }))
-                                        .setTimestamp()
-                                        .setColor(config.embed.color)
-                                        .setFooter(config.embed.footer)
-                                    lastchannel.send(e1)
-                                    message.channel.bulkDelete(7)
-                                })
-                            })
-                        })
-                    }
-                }
-            }
-        }
+module.exports = {
+    name: 'review',
+    description: 'Review a product',
+    options: [{
+            name: 'product',
+            description: 'The product you purchased',
+            type: 'STRING',
+            required: true,
+        },
+        {
+            name: 'review',
+            description: 'The review of the product',
+            type: 'STRING',
+            required: true,
+        },
+        {
+            name: 'rate',
+            description: 'The rate of the product out of ten',
+            type: 'INTEGER',
+            required: true,
+        },
+    ],
+    async run(interaction, client, config, channel) {
+        let product = interaction.options.getString('product');
+        let review = interaction.options.getString('review');
+        let rate = interaction.options.getInteger('rate');
+        let e1 = new MessageEmbed()
+            .setTitle(`A review has been made by ${interaction.user.tag}`)
+            .addFields([{ name: `The Product`, value: `${product}` }, { name: `Review`, value: `${review}` }, { name: `The product it self out of ten`, value: `${rate}` }])
+            .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+            .setColor(config.embed.color)
+            .setFooter({text: config.embed.footer})
+        channel.send({embeds: [e1]})
+        interaction.reply({ content: `Your review has been sent`, ephemeral: true })
     }
 }
-
-module.exports.help = {
-    name: "review",
-    category: "review",
-    aliases: []
-};
--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-const { Client, Collection } = require('discord.js');
-const nat2k15 = new Client();
+const { Client, Collection, Emoji, MessageEmbed } = require('discord.js');
+const nat2k15 = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"] });
+
 const chalk = require('chalk');
 const config = require('./config.json')
 const fs = require('fs')
-const { sep } = require("path");
 nat2k15.prefix = config.prefix;
-["commands", "aliases"].forEach(x => nat2k15[x] = new Collection());
-const commandhandler = (dir = "./commands/") => {
-    fs.readdirSync(dir).forEach(dirs => {
-        const commands = fs.readdirSync(`${dir}${sep}`).filter(files => files.endsWith(".js"));
-        for (const file of commands) {
-            const pull = require(`${dir}/${file}`);
-            if (pull.help && typeof(pull.help.name) === "string" && typeof(pull.help.category) === "string") {
-                if (nat2k15.commands.get(pull.help.name)) return;
-                nat2k15.commands.set(pull.help.name, pull);
-            }
-        }
-    });
-};
-commandhandler();
 
+nat2k15.commands = new Collection()
 nat2k15.once('ready', async() => {
-    console.log(chalk.red `[MADE BY NAT2K15] ` + chalk.white `${nat2k15.user.tag} is now online!`)
+    let commmands = []
+    fs.readdirSync('./commands').filter(file => file.endsWith('.js')).forEach(file => {
+        const command = require(`./commands/${file}`)
+        if (command) {
+            nat2k15.commands.set(command.name, command)
+            commmands.push(command)
+            console.log(chalk.green(`[COMMAND] Loaded ${command.name}`))
+        }
+    })
+    nat2k15.application.commands.set(commmands)
+    console.log(chalk.green `[SUCCESS]` + chalk.white ` ${nat2k15.user.tag} is now online!`)
 })
 
-nat2k15.on('message', async message => {
-    if (message.author.bot) return;
-    let prefix = config.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLowerCase();
-    let args = messageArray.slice(1);
-    //made by NAT2K15
-    if (!message.content.startsWith(prefix)) return;
-    const commandfile = nat2k15.commands.get(cmd.slice(prefix.length));
-    if (commandfile) commandfile.run(nat2k15, message, args);
+
+nat2k15.on('interactionCreate', async(interaction) => {
+    if(interaction.isCommand()) {
+        const command = nat2k15.commands.get(interaction.commandName)
+        if (!command) return;
+        try {
+            let member = interaction.member;
+            let channel = interaction.guild.channels.cache.get(config.review.review_sent)
+            let role = interaction.guild.roles.cache.get(config.review.client_role)
+            if(!role || !channel) return interaction.reply({ content: `Contact the bot owner and tell him to verify his config info. Ther role or channel are invalid`, ephemeral: true })
+            if(member.roles.cache.has(role.id)) {
+                await command.run(interaction, nat2k15, config, channel)
+            } else {
+                let embed = new MessageEmbed()
+                    .setColor(config.embed.color)
+                    .setFooter({text: config.embed.footer})
+                    .setDescription(`You cannot use this command! You are not a client. You must have the ${role} role to use this command`)
+                    .setTimestamp()
+                interaction.reply({ embeds: [embed], ephemeral: true })
+            }
+        } catch (error) {
+            console.log(chalk.red `[ERROR] ` + chalk.white `${error.stack}`)
+        }
+    }
 })
+
 
 nat2k15.login(config.token)
